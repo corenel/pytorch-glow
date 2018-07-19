fedefcb89cc2412d9d6ea4159b087e4e96cfcd18
corenel <xxdsox@gmail.com>
2018-07-19 21:10:04 +0800

    add get_devices()

--- misc/util.py
+++ misc/util.py
@@ -1,18 +1,61 @@
 import os
+import re
 import json
+import torch
 from easydict import EasyDict
 
 
-# def load_profile(setting):
-#     profile_filepath = os.path.join(setting.profile_dir, '{}.json'.format(setting.profile))
-#     if os.path.exists(profile_filepath):
-#         with open(profile_filepath) as f:
-#             conf = json.load(f)
-#             for key in list(conf.keys()):
-#                 setattr(setting, key, conf[key])
-#     return setting
-
 def load_profile(filepath):
+    """
+    Load experiment profile as EasyDict
+
+    :param filepath: path to profile
+    :type filepath: str
+    :return: hyper-parameters
+    :rtype: EasyDict
+    """
     if os.path.exists(filepath):
         with open(filepath) as f:
             return EasyDict(json.load(f))
+
+
+def get_devices(devices):
+    """
+    Get devices for running model
+
+    :param devices: list of devices from profile
+    :type devices: list
+    :return: list of usable devices according to desired and available hardware
+    :rtype: list[str]
+    """
+
+    def parse_cuda_device(device):
+        """
+        Parse device into device id
+
+        :param device: given device
+        :type device: str or int
+        :return: device id
+        :rtype: int
+        """
+        origin = str(device)
+        if isinstance(device, str) and re.search('cuda:([\d]+)', device):
+            device = int(re.findall('cuda:([\d]+)', device)[0])
+        if isinstance(device, int):
+            if 0 <= device <= torch.cuda.device_count() - 1:
+                return device
+        print('Incorrect device "{}"'.format(origin))
+        return
+
+    use_cpu = any([d.find('cpu') >= 0 for d in devices])
+    use_cuda = any([(d.find('cuda') >= 0 or isinstance(d, int)) for d in devices])
+    assert not (use_cpu and use_cuda), 'CPU and GPU cannot be mixed.'
+
+    if use_cuda:
+        devices = [parse_cuda_device(d) for d in devices]
+        devices = [d for d in devices if d is not None]
+        if len(devices) == 0:
+            print('No available GPU found, use CPU only')
+            devices = ['cpu']
+
+    return devices
--- test/test_util.py
+++ test/test_util.py
@@ -1,4 +1,5 @@
 from misc import util
+import torch
 import unittest
 
 
@@ -6,6 +7,23 @@ class TestUtil(unittest.TestCase):
     def test_load_profile(self):
         hps = util.load_profile('profile/celebahq_256x256_5bit.json')
         self.assertIsInstance(hps, dict)
+
+    def test_get_devices(self):
+        # use cpu only
+        devices = ['cpu']
+        self.assertListEqual(devices, util.get_devices(devices))
+        # use gpu
+        cuda_available = torch.cuda.is_available()
+        gpu_count = torch.cuda.device_count()
+        if cuda_available and gpu_count >= 1:
+            self.assertListEqual([0], util.get_devices(['cuda:0']))
+            self.assertListEqual([0], util.get_devices(['cuda:0', 'cuda:1']))
+            self.assertListEqual(['cpu'], util.get_devices(['cuda:1']))
+            with self.assertRaises(AssertionError):
+                util.get_devices(['cuda:1', 'cpu'])
+        if cuda_available and gpu_count >= 2:
+            devices = ['cuda:0', 'cuda:1']
+            self.assertListEqual([0, 1], util.get_devices(devices))
 
 
 if __name__ == '__main__':
